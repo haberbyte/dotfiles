c5091a9749776b0f26cf012b8f2cae74e0b034a3
Jan Habermann <jan.habermann@zendesk.com>
2025-01-03 14:24:54 +0100

    More mac settings

--- mac-settings.sh
+++ mac-settings.sh
@@ -15,3 +15,12 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 2
 # Set dock tile size to 42px
 defaults write com.apple.dock tilesize -int 42
 
+# Disable save to cloud as default
+defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false"
+
+# Disable writing .DS_Store files on USB drives
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Disable writing .DS_Store files on network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
